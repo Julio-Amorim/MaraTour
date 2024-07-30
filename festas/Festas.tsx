430a5caaf2cbe36a6b0f82e7951c9e6400e0c142
Julio Cesar <87783694+Julio-Amorim@users.noreply.github.com>
2024-07-30 12:11:42 -0300

    Add files via upload
    Página de festas

---
+++ festas/Festas.tsx
@@ -0,0 +1,90 @@
+import Header from "../../ components/header/Header";
+import Footer from "../../ components/footer/Footer";
+import "./festas.css";
+
+export const Festas = () => {
+    return (
+        <div>
+            <Header />
+            <main id='main-festas'>
+                <h2>🎉️ • Venha Festejar</h2>
+                <p>
+                    Num espetáculo de cores, danças e ritmos, o Bumba-Meu-Boi é
+                    a expressão máxima da cultura popular do Maranhão. Elevado a
+                    Patrimônio Imaterial do povo brasileiro, essa manifestação
+                    possui origens indefinidas, mas apresenta claros elementos
+                    culturais africanos e europeus, introduzidos principalmente
+                    através da religiosidade. Nas comunidades que realizam a
+                    brincadeira, as celebrações e o trabalho em torno da festa
+                    duram praticamente o ano inteiro. Um dos desafios é preparar
+                    o couro do boi, revestido de camurça belamente decorada com
+                    canutilhos que cobrem o corpo do animal, pois é tradição que
+                    este seja renovado a cada temporada. Durante os festejos
+                    juninos, o Bumba-Meu-Boi reina absoluto, arrastando
+                    multidões e encantando quem assiste pela primeira vez suas
+                    apresentações. A variedade de sotaques, ou ritmos, no
+                    Bumba-Meu-Boi do Maranhão é o que faz toda a diferença.
+                </p>
+                <div className='div-iframe'>
+                    <iframe
+                        width='100%'
+                        height='100%'
+                        src='https://www.youtube.com/embed/t9R65wwe-QM?si=pC1mLQoeEaB05TXm'
+                        title='YouTube video player'
+                        frameborder='0'
+                        allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+                        referrerpolicy='strict-origin-when-cross-origin'
+                        allowfullscreen
+                    ></iframe>
+                </div>
+                <p>
+                    Num espetáculo de cores, danças e ritmos, o Bumba-Meu-Boi é
+                    a expressão máxima da cultura popular do Maranhão. Elevado a
+                    Patrimônio Imaterial do povo brasileiro, essa manifestação
+                    possui origens indefinidas, mas apresenta claros elementos
+                    culturais africanos e europeus, introduzidos principalmente
+                    através da religiosidade. Nas comunidades que realizam a
+                    brincadeira, as celebrações e o trabalho em torno da festa
+                    duram praticamente o ano inteiro. Um dos desafios é preparar
+                    o couro do boi, revestido de camurça belamente decorada com
+                    canutilhos que cobrem o corpo do animal, pois é tradição que
+                    este seja renovado a cada temporada. Durante os festejos
+                    juninos, o Bumba-Meu-Boi reina absoluto, arrastando
+                    multidões e encantando quem assiste pela primeira vez suas
+                    apresentações. A variedade de sotaques, ou ritmos, no
+                    Bumba-Meu-Boi do Maranhão é o que faz toda a diferença.
+                </p>
+
+                <p>
+                    No Maranhão, o período junino é caracterizado por uma
+                    explosão de sons, cores e sabores. Esse é o momento de
+                    celebrar as ricas manifestações culturais maranhenses, com
+                    ênfase no Bumba-Meu-Boi. São Luís se transforma em um grande
+                    arraial, com diversas estruturas espalhadas por toda a ilha
+                    para apresentações culturais, danças típicas e cantores
+                    regionais, além das barracas oferecendo comidas típicas.
+                </p>
+                <p>
+                    Celebrada ao longo de todo o mês de maio desde os tempos
+                    coloniais, a Festa do Divino Espírito Santo é uma fascinante
+                    combinação de devoção religiosa e homenagens ao Império. O
+                    "imperador" e sua corte, vestidos com trajes típicos,
+                    visitam as casas dos festeiros durante as comemorações.
+                </p>
+                <div className='div-iframe'>
+                    <iframe
+                        width='100%'
+                        height='100%'
+                        src='https://www.youtube.com/embed/LrtfEJJ-z5M?si=PSHmq6y0wJ7dmGBE'
+                        title='YouTube video player'
+                        frameborder='0'
+                        allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+                        referrerpolicy='strict-origin-when-cross-origin'
+                        allowfullscreen
+                    ></iframe>
+                </div>
+            </main>
+            <Footer />
+        </div>
+    );
+};
