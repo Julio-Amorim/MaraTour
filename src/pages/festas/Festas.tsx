c56541b7201fee9c7fed5184537f4da2fa2b5a5f
Julio Cesar <juliocesar81632@gmail.com>
2024-07-30 12:17:26 -0300

    feat: Corrigir atributos de iframe na página de festas
    Corrige os atributos de iframe na página de festas para seguir as convenções de nomenclatura corretas.

--- src/pages/festas/Festas.tsx
+++ src/pages/festas/Festas.tsx
@@ -31,10 +31,10 @@ export const Festas = () => {
                         height='100%'
                         src='https://www.youtube.com/embed/t9R65wwe-QM?si=pC1mLQoeEaB05TXm'
                         title='YouTube video player'
-                        frameborder='0'
+                        frameBorder='0'
                         allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
-                        referrerpolicy='strict-origin-when-cross-origin'
-                        allowfullscreen
+                        referrerPolicy='strict-origin-when-cross-origin'
+                        allowFullScreen
                     ></iframe>
                 </div>
                 <p>
@@ -77,10 +77,10 @@ export const Festas = () => {
                         height='100%'
                         src='https://www.youtube.com/embed/LrtfEJJ-z5M?si=PSHmq6y0wJ7dmGBE'
                         title='YouTube video player'
-                        frameborder='0'
+                        frameBorder='0'
                         allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
-                        referrerpolicy='strict-origin-when-cross-origin'
-                        allowfullscreen
+                        referrerPolicy='strict-origin-when-cross-origin'
+                        allowFullScreen
                     ></iframe>
                 </div>
             </main>
